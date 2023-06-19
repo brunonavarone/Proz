e335f669be28f6ab42a1656b26afc17dd206a2bd
Bruno <brunohenrique.travessa@gmail.com>
2023-06-19 13:10:47 -0300

    upando a pasta da atividade 03 de banco de dados

---
+++ Atividades_CodePark/Atividade_BDII_03/Procedures.sql
@@ -0,0 +1,26 @@
+CREATE TABLE TabelaCompras (
+    ID SERIAL PRIMARY KEY,
+    Produto VARCHAR(100),
+    DataCompra DATE
+);
+
+INSERT INTO TabelaCompras (Produto, DataCompra)
+VALUES
+    ('Produto A', '2023-06-19'),
+    ('Produto B', '2023-06-19'),
+    ('Produto C', '2023-06-18'),
+    
+CREATE OR REPLACE PROCEDURE LevantamentoDiarioProdutos()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    SELECT DATE(DataCompra) AS Data, COUNT(*) AS Quantidade
+    FROM TabelaCompras
+    GROUP BY DATE(DataCompra);
+END;
+$$;
+
+    ('Produto D', '2023-06-18'),
+    ('Produto E', '2023-06-18'),
+    ('Produto F', '2023-06-17'),
+    ('Produto G', '2023-06-17');
